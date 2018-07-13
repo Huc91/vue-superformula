59043970db109b678d543f56c67f3df083bc90d9
Huc <luca.ucciero@gmail.com>
2018-07-13 17:09:27 +0200

    first commit

---
+++ src/components/SuperFormula.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="super-formula-container">
+  </div>
+</template>
+
+<script>
+function superFormula(){
+
+  const N1 = 1,
+        N2 = 1,
+        N3 = 1,
+        A  = 1,
+        B  = 1,
+        C  = 1;
+  function superShape( angle ){
+
+    let r = 1;
+    return r;
+    
+  }
+
+  function createPoints(){
+
+  }
+}
+export default {
+  name: 'SuperFormula',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
